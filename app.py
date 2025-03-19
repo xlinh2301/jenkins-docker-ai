ade789fbbb6384887b8683fa53459d9d52ec6744
nxlinh2301 <nxlinh2301@gmail.com>
2025-03-19 01:26:11 +0000

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from AI Model 12345!"
+    return "Hello from AI Model 125!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
